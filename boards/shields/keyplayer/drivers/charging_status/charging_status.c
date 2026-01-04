43502bcf0828a14da951d455eddec92b482b7a72
E_huang <176415120+E4181@users.noreply.github.com>
2026-01-04 21:34:22 +0800

    Update charging_status.c

--- boards/shields/keyplayer/drivers/charging_status/charging_status.c
+++ boards/shields/keyplayer/drivers/charging_status/charging_status.c
@@ -6,7 +6,7 @@
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/logging/log.h>
-#include <drivers/charging_status.h>
+#include "charging_status.h"
 
 LOG_MODULE_REGISTER(charging_status, CONFIG_CHARGING_STATUS_LOG_LEVEL);
 
